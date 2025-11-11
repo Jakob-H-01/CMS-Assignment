df143d50b539d2b1af6c64d2142ede09d2ad11ea
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-11-11 12:18:10 +0100

    Add Contact Section

--- CMS-Assignment/umbraco/models/ProjectDetail.generated.cs
+++ CMS-Assignment/umbraco/models/ProjectDetail.generated.cs
@@ -50,6 +50,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Sections
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.3.4+0d2393d")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("projectDetailAdditionalSections")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel ProjectDetailAdditionalSections => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "projectDetailAdditionalSections");
+
+		///<summary>
 		/// Date
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.3.4+0d2393d")]
